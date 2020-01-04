20ed7bbcf531494be0a622142bab339333b77619
柠檬儒Sensei <replicasensei@ningmengruSenseideMacBook-Pro.local>
2020-01-05 01:53:18 +0800

    第三次

---
+++ src/main/java/com/mamoru/chileme/controller/SellerOrderController.java
@@ -0,0 +1,45 @@
+package com.mamoru.chileme.controller;
+
+
+import com.mamoru.chileme.dto.OrderDTO;
+import com.mamoru.chileme.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+/**
+ * 卖家端订单
+ */
+@Controller
+@RequestMapping("/seller/order")
+public class SellerOrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    /**
+     * 订单列表
+     * @param page 第几页，从1页开始
+     * @param size 一页有多少条数据
+     * @return
+     */
+
+    @GetMapping("/list")
+    public ModelAndView list(@RequestParam(value = "page", defaultValue = "1") Integer page,
+                             @RequestParam(value = "size", defaultValue = "10") Integer size,
+                             Map<String, Object> map) {
+        PageRequest request = new PageRequest(page, size);
+        Page<OrderDTO> orderDTOPage = orderService.findList(request);
+        map.put("orderDTOPage", orderDTOPage);
+        return new ModelAndView("order/list", map);
+    }
+
+
+}
--- src/main/java/com/mamoru/chileme/service/impl/OrderServiceImpl.java
+++ src/main/java/com/mamoru/chileme/service/impl/OrderServiceImpl.java
@@ -208,4 +208,15 @@ public class OrderServiceImpl implements OrderService {
 
         return orderDTO;
     }
+
+    @Override
+    public Page<OrderDTO> findList(Pageable pageable) {
+        Page<OrderMaster> orderMasterPage = orderMasterDao.findAll(pageable);
+
+        List<OrderDTO> orderDTOList = OrderMaster2OrderDTOConverter.convert(orderMasterPage.getContent());
+
+        return new PageImpl<>(orderDTOList, pageable, orderMasterPage.getTotalElements());
+    }
+
+
 }
