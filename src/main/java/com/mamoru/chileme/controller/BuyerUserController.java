eefd5d77d4b8379557ed817412222be5183122cc
柠檬儒Sensei <replicasensei@ningmengruSenseideMacBook-Pro.local>
2020-01-31 00:49:00 +0800

    部署

--- src/main/java/com/mamoru/chileme/controller/BuyerUserController.java
+++ src/main/java/com/mamoru/chileme/controller/BuyerUserController.java
@@ -1,6 +1,5 @@
 package com.mamoru.chileme.controller;
 
-import com.mamoru.chileme.config.ProjectUrlConfig;
 import com.mamoru.chileme.constant.CookieConstant;
 import com.mamoru.chileme.constant.RedisConstant;
 import com.mamoru.chileme.entity.BuyerInfo;
@@ -18,8 +17,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
